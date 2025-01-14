28245263e8fdf73c68e35b590f015da44477e6c5
24LP9558 24LP9558 <tudewdagwagantulga@gmail.com>
2025-01-14 11:27:29 +0800

    boljinshu

--- src/App.js
+++ src/App.js
@@ -70,38 +70,48 @@ function App() {
   }, []);
 
   return (
-    <div className={`min-h-screen flex items-center justify-center`}>
-      <div className="max-w-sm w-full bg-white rounded-lg shadow-lg p-6 space-y-6">
-        <h1 className="text-center text-4xl font-semibold text-gray-800">
-          Weather App
-        </h1>
-
-        <input
-          onChange={handleChange}
-          placeholder="Enter city name"
-          className="w-full px-4 py-2 text-lg rounded-full bg-gray-100 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-        <div className="overflow-y-auto max-h-64">
-          {countriesSearch.length > 0 &&
-            filteredData.map((country, index) => {
-              return <div key={index}>{country}</div>;
-            })}
-        </div>
-        {loading && <div>Loading...</div>}
-        {weatherData && weatherData.location && weatherData.current && (
-          <div>
-            <h2>Weather in {weatherData.location.name}</h2>
-            <p>Temperature: {weatherData.current.temp_c}°C</p>
-            <p>Weather: {weatherData.current.condition.text}</p>
+    <div className="min-h-screen flex items-center justify-center relative">
+      <section className="flex flex-1 relative items-center justify-center w-1/2 h-screen">
+        <div className="flex relative w-[414px] h-[828px] justify-center z-10">
+          <div className="z-20 w-full h-5/6 rounded-[10.5px] overflow-hidden shadow-lg bg-white/75">
+            <div className="space-y-12 px-10 py-14 backdrop-blur-lg flex items-center justify-center">
+              <div className="flex items-center justify-center w-72 h-72">
+                <img
+                  className="absolute object-cover w-72 h-72"
+                  src="https://pinecone-academy-weather-app.vercel.app/_next/image?url=%2Fsun.png&w=640&q=75"
+                />
+              </div>
+            </div>
+            <div className="px-12"></div>
           </div>
-        )}
-        <button
-          onClick={fetchData}
-          className="w-full py-2 mt-4 bg-blue-600 rounded-full text-lg font-semibold focus:outline-none hover:bg-blue-500 transition-all"
-        >
-          Get Weather
-        </button>
+          <div></div>
+        </div>
+      </section>
+      <div className="absolute w-full z-30 top-0 left-0 flex items-center justify-center h-full">
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Search"
+          className="px-4 py-2 w-60 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        />
       </div>
+      <section className="flex flex-1 relative items-center justify-center w-1/2 h-screen bg-[rgb(15,20,30)]">
+        <div className="flex relative w-[414px] h-[828px] justify-center z-10">
+          <div className="z-20 w-full h-5/6 rounded-[10.5px] overflow-hidden shadow-lg bg-[#111827]/75 backdrop-blur-md">
+            <div className="space-y-12 px-10 py-14 backdrop-blur-lg flex items-center justify-center">
+              <div className="flex justify-between items-center"></div>
+              <div className="flex items-center justify-center w-72 h-72">
+                <img
+                  className="absolute object-cover w-72 h-72"
+                  src="https://pinecone-academy-weather-app.vercel.app/_next/image?url=%2Fmoon.png&w=640&q=75"
+                />
+              </div>
+            </div>
+            <div className="px-12"></div>
+          </div>
+          <div></div>
+        </div>
+      </section>
     </div>
   );
 }
